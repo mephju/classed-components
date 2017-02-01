8f38d3d56f07a5973c5aa9afb22151734286175c
Kolja <kolja.kirchner@datatrans.ch>
2017-02-01 15:35:40 +0100

    refactor for more prettiness

--- src/classed/classed.js
+++ src/classed/classed.js
@@ -1,36 +1,6 @@
 import domElements from './utils/domElements'
-import {createElement, Component} from 'react'
+import ClassedFactory from './ClassedFactory'
 
-// classed.div`
-//  hello
-//  love
-//  more ${true}
-// `
-
-const toResult = props =>
-  interpol => typeof interpol === 'function'
-  ? interpol(props)
-  : interpol
-
-const interpolate = (strings, interpolations = [], props) => {
-  const results   = interpolations.map(toResult(props))
-  const className = strings.reduce((className, string, i) => className + string + results[i], '')
-  return className
-}
-
-const ClassedFactory = (tag) => (strings, interpolations) => {
-
-  const className = strings.join(' ')
-
-  return props => {
-
-    const className = interpolate(strings, interpolations, props)
-    // const p = {...this.props}
-    const p = {className, ...props}
-    return createElement(tag, p, p.children)
-  }
-
-}
 
 const addTagComponent = (classed, tag) => {
   classed[tag] = ClassedFactory(tag)
@@ -39,8 +9,6 @@ const addTagComponent = (classed, tag) => {
 const Classed = () => {
 
   const classed = () => {}
-
-
 
   domElements.forEach(addTagComponent.bind(null, classed))
 
--- src/classed/components.js
+++
@@ -1,8 +0,0 @@
-import domElements from './utils/domElements'
-import React, {PropTypes, Component} from 'react'
-
-
-
-const components = domElements.map(tagName => {
-  return (...args) => <[tagName]> args[0].children </[tagName]>
-})
