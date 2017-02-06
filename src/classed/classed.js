bab8048526333df37985ae20d2c52d3eecbb04c0
Kolja <kolja.kirchner@datatrans.ch>
2017-02-06 08:51:18 +0100

    remove comment

--- src/classed/classed.js
+++ src/classed/classed.js
@@ -3,21 +3,6 @@ import last from './utils/last'
 import buildTagFunction from './buildTagFunction'
 
 
-// const MyComponent = classed.div`
-//   hello ${props => props.needsDefault}
-//   hello2
-//   ${props => 'hello2'}
-//   ${p => p.needsDefault ? 'hello' : 'hello2'}
-// `
-
-// const MyComponent2 = classed(MyComponent)`
-//   evenMoreClasses
-//   yeah
-// `
-// const MyComponent3 = classed(MyComponent2)`moooore`
-//
-
-
 
 
 const classed = (ClassedComponent) => {
